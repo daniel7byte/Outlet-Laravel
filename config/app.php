413cf882ea9dc3e0cb7c009512a477d92e99a1ab
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2016-10-12 20:31:46 -0500

    Laravel Debugbar

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
 
         // Mis provider
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -232,6 +233,7 @@ return [
         // Mis aliases
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
